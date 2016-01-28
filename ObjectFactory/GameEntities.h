8673bdfc007dfce5ac2cb16a8427cb4381b83538
theDrake <theDrake@users.noreply.github.com>
2016-01-28 16:35:18 -0700

    Add ObjectFactory files

---
+++ ObjectFactory/GameEntities.h
@@ -0,0 +1,15 @@
+#ifndef GAMEENTITIES_H_
+#define GAMEENTITIES_H_
+
+class GameEntity {
+ public:
+  virtual ~GameEntity() {}
+  virtual const char *GetClassName() { return "GameEntity"; }
+};
+
+class GameCamera : public GameEntity {
+ public:
+  virtual const char *GetClassName() { return "GameCamera"; }
+};
+
+#endif  // GAMEENTITIES_H_
