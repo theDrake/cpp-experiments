9d2f127666962e3a19eb0ee1ddd221b87cea211b
David C. Drake <theDrake@users.noreply.github.com>
2020-04-12 01:57:03 +0800

    Add experiment.cc

---
+++ experiment.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  using namespace std;
+
+  cout << "Come up and C++ me \nsometime.\n";
+  cout << endl;
+  cout << "You won't \nregret it!" << endl;
+
+  return 0;
+}
